73573c428419640bd02dc065f315398270ea4bb9
Jevon Putra <jevonop88@gmail.com>
2024-12-23 22:30:03 +0700

    INIT-PROJECT implementation route

--- app/src/main/java/com/jop/learncompose/MainActivity.kt
+++ app/src/main/java/com/jop/learncompose/MainActivity.kt
@@ -7,14 +7,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.jop.learncompose.ui.routes.NavigationRoute
 import com.jop.learncompose.ui.theme.LearnComposeTheme
-import dagger.hilt.EntryPoint
+import dagger.hilt.android.AndroidEntryPoint
 
-@EntryPoint
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,28 +20,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             LearnComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    NavigationRoute(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LearnComposeTheme {
-        Greeting("Android")
     }
 }
